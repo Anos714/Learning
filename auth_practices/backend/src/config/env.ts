602fbf54e8ded9c3c3c3c068e0ac673aafc5dea3
Anos714 <sainrahul374@gmail.com>
2026-04-02 21:28:47 +0530

    feat:setup all my env variables in env.ts config file

--- auth_practices/backend/src/config/env.ts
+++ auth_practices/backend/src/config/env.ts
@@ -3,4 +3,15 @@ import "dotenv/config";
 export const env = {
   PORT: Number(process.env.PORT) || 8000,
   MONGO_URI: process.env.MONGO_URI || "",
+  NODE_ENV: process.env.NODE_ENV || "development",
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET || "",
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || "",
+  SMTP_HOST: process.env.SMTP_HOST || "",
+  SMTP_PORT: Number(process.env.SMTP_PORT) || 587,
+  SMTP_USER: process.env.SMTP_USER || "",
+  SMTP_PASS: process.env.SMTP_PASS || "",
+  EMAIL_FROM: process.env.EMAIL_FROM || "",
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "",
+  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL || "",
 };
